3cb553b84603f2ac532a29141e199f1088001ab2
Priyanka Kumari <priyanka.kumari@unifize.com>
2024-12-05 16:03:14 +0530

    first commit

---
+++ test/csinterpreter/core_test.clj
@@ -0,0 +1,7 @@
+(ns csinterpreter.core-test
+  (:require [clojure.test :refer :all]
+            [csinterpreter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
